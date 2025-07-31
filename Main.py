e44c0a4c1e96f6a4b0652bc3409166aaa0e0645f
WillowWisp <zcrawshaw@gmail.com>
2025-07-31 23:29:40 +0000

    modified:   Main.py Added win conditions. Now i feel bad for my villain.

--- Main.py
+++ Main.py
@@ -1,6 +1,9 @@
 from colorama import Fore, Back , Style
 from Modules import main_room
-
+'''
+i coded 80% of this in one go without testing most of it (thanks github codespaces for making a good chunk of testing impossible)
+if this works at all I'll be shocked 
+'''
 #list of all rooms and which module to run for them
 rooms = {
     "main_room": main_room.main_room_description(),
@@ -25,7 +28,7 @@ print(Fore.RED , "First, What is your name?" , Style.RESET_ALL)
 #name = input().strip().lower()
 name = "archy" #name for testing purposes TODO remove this line when done testing
 
-# little easter egg for archy (A character in my games)
+# little easter egg for archy (A joke character i use alot take a look at my youtube TODO add youtube. Archy will be appearing soon)
 if name == "archy":
     print(Fore.RED , "Welcome back, Archy!/n It's been so long since we've played together" , Style.RESET_ALL)
     hp = 9999999999999999
@@ -49,3 +52,20 @@ while win == False:
     else: 
     print(Fore.RED , "Another victim for the horde" , Style.RESET_ALL)
     print(Fore.RED , Back.WHITE , "Game Over." , Style.RESET_ALL)
+else: 
+    print(Fore.RED , "Thank you" , Style.RESET_ALL)
+    if "dusty book" and "lore book 1" in inventory:
+        print("Gurerren the deciever, lost to the madness of his crypt locked away any who entered.")
+        print("His fractured mind convincing him the only way to save his people was to lock them away.")
+        print("With the help of the captives you have freed, you have defeated Gurerren and freed the captives.")
+        print("Ending his life, and the curse holding him.")
+        print(Fore.WHITE , "The End." , Style.RESET_ALL)
+    elif "dusty book" in inventory and "lore book 1" not in inventory:
+        print("You have defeated the cruel Gurerren, his monsterous form now lifeless.")
+        print("the captives you have freed are now free to leave.")
+        print(Fore.WHITE , "The End?" , Style.RESET_ALL)
+    elif "lore book 1" in inventory and "dusty book" not in inventory:
+        print("You have defeated the cruel Gurerren, his monsterous form now lifeless.")
+        print("The once peaceful man now a monster of his own making. His mind fractured by the curse of his crypt.")
+        print("Having abducted locals of the village to feed his curse, they seem to be free now.")
+        print(Fore.WHITE , "The End?" , Style.RESET_ALL)
