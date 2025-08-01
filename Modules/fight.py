a7743d7cbb01c5c3ee3ccac4dd56fff55086a80d
WillowWisp <zcrawshaw@gmail.com>
2025-08-01 17:41:20 +0000

    new file:   Modules/fight.py 	modified:   Modules/right_room.py added stuff

---
+++ Modules/fight.py
@@ -0,0 +1,20 @@
+import math
+
+boss_stats = {
+    hp = 500
+    attack = 50
+    defence = 20
+}
+
+ghoul1_stats = {
+    hp = 200
+    attack = 30
+    defence = 10
+}
+
+def attack(attack , enemy , enemy_stats):
+    enemy_stats = enemy
+    damage = attack - enemy_stats["defence"]
+    if damage > 0:
+        enemy_stats["hp"] -= damage
+    return enemy_stats
---
+++ Modules/right_room.py
@@ -0,0 +1,10 @@
+
+def right_room_description:
+    print()
+
+def right_room_actions(inventory, current_room, hp , defence , attack):
+    actions = {
+        "attack" : 
+        "block" :
+        "spare" : 
+    }
